89a181a6ced3176a9b95b2894d8f78a5891468e3
Nestor Heredia <nestor.heredia@correounivalle.edu.co>
2025-05-10 09:04:20 -0500

    Implement appointment model with Room database and save icon
    - Created Appointment.kt data class
    - Added AppointmentDao.kt interface for Room operations
    - Set up AppointmentDB.kt as the Room database
    - Updated Constants.kt with necessary constants
    - Added ic_save.xml drawable icon

---
+++ app/src/main/java/com/example/petsapplication/view/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.petsapplication.view.utils
+
+object Constants {
+    const val NAME_BD : String = "myagendapp.data.db"
+    const val BASE_URL="https://dog.ceo/api/"
+    const val END_POINT_IMAGES="breeds/image/random"
+    const val END_POINT_BREEDS="breeds/list/all"
+}
